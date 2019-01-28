57e29c1260e80c05cb124c644085205b00f04ebf
ronyef <rony.efendy@gmail.com>
2019-01-28 20:37:21 +0700

    edit .gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAtkHYcirnWfsIcAQ-4x68rhMhUYc0S4T4",
+        authDomain: "hazard-rep.firebaseapp.com",
+        databaseURL: "https://hazard-rep.firebaseio.com",
+        projectId: "hazard-rep",
+        storageBucket: "hazard-rep.appspot.com",
+        messagingSenderId: "816808563131"
+    },
+    googleMapApiKey: 'AIzaSyAbMejVUUM7xt2a6V7vVRykRyzu3JDVyD4'
+};
